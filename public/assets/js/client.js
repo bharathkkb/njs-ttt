c476d9481c6fef74714e333a0d1ec7ed8f551f6c
bharathkkb <bharathkrishnakb@gmail.com>
2015-03-23 02:46:30 +0530

    Set up  basic login to test login. xxx is the pass

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -2,15 +2,24 @@ var socket=io();
 
 //get key
 var input="";
-var key='xxx';
+var key="";
+var form=$('form.login'),
+passbox=form.find('input[type=text]');
+
+form.submit(function(e){
+    e.preventDefault();
+    key=passbox.val().trim();
+    if(key.length){
 
 socket.emit('load',{
     key:key
 });
+    }
+});
 socket.on('access', function(data){
     if(data.access === "granted"){
         //hide key dialog
-        window.alert("hi");
+        window.alert("xoxo");
         socket.emit('input', {
             input: input,
                 key: key
