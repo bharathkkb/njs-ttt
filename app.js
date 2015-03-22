44f2a55d99f2e05fed652a8a8dec8c45a8d46d93
bharathkkb <bharathkrishnakb@gmail.com>
2015-03-23 02:35:52 +0530

    got the barebones njs server up and running

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 var express = require('express');
-app= express();
+app = express();
 var port = process.env.PORT || 8080;
-var io=require('socket.io').listen(app.listen(port));
-app.use(express.static(__dirname+'/public'));
-var pass= ' xxx';
-var tic=io.on('connection',function(socket){
-	socket.on('load',function(data){
-     socket.emit('access',{
-     	access:(data.key === pass ? "granted":"denied")
-     });
-		
+var io = require('socket.io').listen(app.listen(port));
+app.use(express.static(__dirname + '/public'));
+var pass = 'xxx';
+var tic = io.on('connection', function (socket) {
+    socket.on('load', function (data) {
+        socket.emit('access', {
+            access: (data.key === pass ? "granted" : "denied")
+        });
 
-});
-    
-    socket.on('inp',function(data){
-        if(data.key===pass){
-            tic.emit('newinp',{
+
+    });
+
+    socket.on('input', function (data) {
+        if (data.key === pass) {
+            tic.emit('newinp', {
                 input: data.input
             });
         }
     });
 });
-
--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -2,11 +2,17 @@ var socket=io();
 
 //get key
 var input="";
+var key='xxx';
+
+socket.emit('load',{
+    key:key
+});
 socket.on('access', function(data){
     if(data.access === "granted"){
         //hide key dialog
-        socket.emit('inp', function(data){
+        window.alert("hi");
+        socket.emit('input', {
             input: input,
                 key: key
         });
-    }}
+    }});
