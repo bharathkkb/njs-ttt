4e30c4fa59fa908256c69a82643f8347913a8bbd
bharathkkb <bharathkrishnakb@gmail.com>
2015-03-25 03:34:39 +0530

    Passing the value to the game function.
    Working alright.
    Some changes needed, like turn based restrictions, diff color for each
    player maybe and stuff.

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -22,7 +22,7 @@ socket.on('access', function (data) {
     if (data.access === "granted") {
 
         if (inp != 999) {// inp is the input being passed
-            window.alert("Send input");
+            
             socket.emit('inp', {
                 input: inp,
                 key: key
@@ -31,7 +31,8 @@ socket.on('access', function (data) {
 
         socket.on('recinp', function (data) {
             var recin = data.input;
-            window.alert(recin);
+            //passing value onto the game function;
+            canvasClicked(recin);
 
 
         });
